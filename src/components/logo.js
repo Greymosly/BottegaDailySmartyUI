5750e78b3e83ccf4b5b528456da47aa2f5f32b1d
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-04-14 14:38:22 -0600

    modified Log component to take in custom size via props

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
- 
+import React, { Component } from "react";
+
 export default class Logo extends Component {
   render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
     return (
       <div className="logo-main">
-          <img src="/assets/Logo/ds_circle_logo.png"/>
+        <img style={size} src="/assets/Logo/ds_circle_logo.png" />
       </div>
     );
   }
