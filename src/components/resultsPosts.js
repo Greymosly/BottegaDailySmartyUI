0242509c03f6b4de80fd965ad6e2a33352d7aa66
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-04-15 12:39:06 -0600

    put together resultsPosts component

---
+++ src/components/resultsPosts.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ResultsPosts extends Component {
+    render() {
+        return(
+            <div className="results-posts">
+                <div className="results-posts-wrapper">
+                    <ul className="results-posts-posts">
+                        RESULTS GO HERE
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ResultsPosts;
